65daae64787fd0496c3a86d49367ffe62b551a2c
Douglas caina <douglas.da.silva@hexad.de>
2019-03-13 10:00:45 +0100

    02

--- src/scene/chat/chat.scene.js
+++ src/scene/chat/chat.scene.js
@@ -22,11 +22,41 @@ class ChatScene extends React.Component {
                                     )
                                 })
                             }
-
                         </li>
                     </ul>
                 </ContactsSection>
-                <ChatSection>hey</ChatSection>
+                <ChatSection>
+                    <ChatContentHeader>
+                        <div>
+                            <h3>Christian Martinez</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </ChatContentHeader>
+                    <ChatContent>
+                        <ChatEntry>
+                            <ContactPicture src="https://bit.ly/2K4WKzX"/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac accumsan odio, eleifend
+                                mollis urna. Donec euismod lorem nec enim vulputate, a mollis est blandit. Proin in
+                                lectus lectus. Donec efficitur risus ut dictum lobortis. Vivamus gravida, enim nec
+                                sodales euismod, nunc turpis euismod ligula, nec varius nulla erat sagittis ipsum. In
+                                rhoncus libero eu libero varius, a vestibulum augue mollis. Nam at nisl massa</p>
+                        </ChatEntry>
+                        <ChatEntry>
+                            <ContactPicture src="https://bit.ly/2K4WKzX"/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac accumsan odio, eleifend
+                                mollis urna. Donec euismod lorem nec enim vulputate</p>
+                        </ChatEntry>
+                        <ChatEntry>
+                            <ContactPicture src="https://bit.ly/2K4WKzX"/>
+                            <p>
+                                rhoncus libero eu libero varius, a vestibulum augue mollis. Nam at nisl massa</p>
+                        </ChatEntry>
+                    </ChatContent>
+                    <ChatInput>
+                        <input type="text"/>
+                        <button>Send</button>
+                    </ChatInput>
+                </ChatSection>
             </ContainerSection>
         )
     }
@@ -44,12 +74,16 @@ const ContactsSection = styled.div`
     align-items: flex-start;
     background-color: #FAFAFC;
     min-height: 100%;
+    min-width: 300px;
     overflow: scroll;
 `;
 
 const ChatSection = styled.div`
   display: flex;
+  flex-direction: column;
+  padding-bottom: 60px;
   flex-grow: 1;
+  position: relative;
 `;
 
 const ContactRow = styled.div`
@@ -82,9 +116,76 @@ const ContactName = styled.div`
 `;
 
 const ContactPicture = styled.img`
-  max-width: 50px;
-  max-height: 50px;
   border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+
+const ChatInput = styled.div`
+  position: absolute;
+  height:50px;
+  bottom: 10px;
+  left: 0;
+  right: 0;
+  input {
+    height: 50px;
+    width: 80%;
+    font-size: 18px;
+    font-family: Helvetica, Serif;
+    color: #727274;
+  }
+  button {
+    border-radius: 5px;
+    border: 1px solid #31ABF4;
+    padding: 20px;
+    color: white;
+    background-color: #31ABF4;
+  }
+`;
+
+const ChatContent = styled.div`
+  display:flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: flex-end;
+`;
+
+const ChatContentHeader = styled.div`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  padding: 0 15px;
+  border-bottom: 1px solid #F4F5F6;
+    
+    h3 {
+        color: #727274;
+        font-weight: 500;
+        margin: 0;
+        padding: 0;
+    }
+    p {
+        padding: 0;
+        margin: 0;
+        font-weight: normal;
+        color: #C4C4C4;
+    }
+`;
+
+const ChatEntry = styled.div`
+  display:flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 10px;
+  p {
+    margin: 0 0 0 10px;
+    background: #31ABF4;
+    color: white;
+    padding: 10px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }  
 `;
 
 export default ChatScene;
